04fbf2453018b87a2fc2701004b8f60f1a3a3e1b
chromatic <chromatic@wgz.org>
2014-01-27 21:54:00 +0000

    Import of CHROMATIC/Finance-Dogecoin-1.20140127.2153 from CPAN.
    gitpan-cpan-distribution: Finance-Dogecoin
    gitpan-cpan-version:      1.20140127.2153
    gitpan-cpan-path:         CHROMATIC/Finance-Dogecoin-1.20140127.2153.tar.gz
    gitpan-cpan-author:       CHROMATIC
    gitpan-cpan-maturity:     released

---
+++ lib/Finance/Dogecoin.pm
@@ -0,0 +1,30 @@
+package Finance::Dogecoin;
+{
+  $Finance::Dogecoin::VERSION = '1.20140127.2153';
+}
+
+use strict;
+use warnings;
+
+use Finance::Dogecoin::API;
+
+'to the moon';
+__END__
+=pod
+
+=head1 NAME
+
+Finance::Dogecoin - use the dogeapi.com API from Perl
+
+=head1 SYNOPSIS
+
+See L<Finance::Dogecoin::API>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2014 chromatic, some rights reserved.
+
+This program is free software. You can redistribute it and/or modify it under
+the same terms as Perl 5.18.
+
+=cut
